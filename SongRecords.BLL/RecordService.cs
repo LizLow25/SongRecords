48efdf0c8789ebe30c69fdc1f3114ad8154261fb
JamesZ7 <james7.ca@gmail.com>
2023-09-14 15:19:13 -0400

    load update 0.5

--- SongRecords.BLL/RecordService.cs
+++ SongRecords.BLL/RecordService.cs
@@ -1,4 +1,6 @@
 ﻿using SongRecordStore.CORE.Interfaces;
+using SongRecordStore.CORE.Models;
+using System.Data;
 
 namespace SongRecordStore.BLL
 {
@@ -12,5 +14,26 @@ namespace SongRecordStore.BLL
             this.repo = repo;
         }
 
+        public Result<SongRecord> Get(string songName)
+        {
+            Result<SongRecord> result = new Result<SongRecord>();
+            Result<List<SongRecord>> recordResult = _repo.ReadAll();
+            if (!recordResult.Success)
+            {
+                result.Message = recordResult.Message;
+                return result;
+            }
+            foreach (SongRecord record in recordResult.Data)
+            {
+                if (record.Name == songName)
+                {
+                    result.Data = record;
+                    result.Success = true;
+                    return result;
+                }
+            }
+            result.Message = $"No song record found for {songName}";
+            return result;
+        }
     }
 }
